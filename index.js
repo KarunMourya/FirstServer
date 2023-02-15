f3265c28f0418866ddca9178cd4092e801e926f5
KarunMourya <70096172+KarunMourya@users.noreply.github.com>
2023-02-15 17:46:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const fs = require('fs')
 const dotenv = require('dotenv')
 dotenv.config();
-server = http.createServer(function(req,res){
+const server = http.createServer(function(req,res){
   res.writeHead(200,{'Content-Type':'text/html'})
   fs.readFile('./build/index.html',function(error,data){
     if(error){
